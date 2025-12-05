1d419a6be1d3f181739bc442a560c54d68d18f89
German <germanmendoza03@gmail.com>
2025-12-05 14:59:21 -0300

    algo de cors (nada)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,29 +4,25 @@ import usuarioRoutes from "./api/usuario/usuarioRoutes";
 import authRoutes from "./api/auth/authRoutes";
 import "reflect-metadata";
 import cors from "cors";
-var cookieParser = require('cookie-parser')
-
+var cookieParser = require('cookie-parser');
 
 const app = express();
 
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://inscripcion-ferias-5vsz7xcf4-germanmdzs-projects.vercel.app"
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000"
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/feria", feriaRoutes);
 app.use("/usuario", usuarioRoutes);
 app.use("/auth", authRoutes);
-// curl en powershell
-// curl -Uri "http://localhost:3000/ferias/crear" -Method POST -Headers @{ "Content-Type" = "application/json" } -Body '{ "nombre": "Test2" }'
-
-// curl en cmd
-// curl -X POST "http://localhost:3000/ferias/crear" -H "Content-Type: application/json" -d "{\"nombre\":\"Test\"}"
 
 export default app;
