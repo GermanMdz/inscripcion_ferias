efba08cacee20cb92ad45418c8d41452871d0e96
German <germanmendoza03@gmail.com>
2026-03-02 14:39:03 -0300

    ipv4 en supabase

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { FeriaEntity } from "./infra/entities/feriaEntity";
@@ -9,12 +12,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST!,
   port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "admin",
-  database: process.env.DB_NAME || "inscripcion_ferias",
-  // synchronize: true, // usar true solo en desarrollo
+  username: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
   logging: false,
   entities: [FeriaEntity, UsuarioEntity, InscripcionEntity],
   migrations: [__dirname + "/migrations/*.{ts,js}"],
